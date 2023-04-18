6f92f9a0e1628b1978bdc9899b25000bb541e8ee
rahmad310317 <rahmadwindisimanullang@gmail.com>
2023-04-18 23:38:12 +0700

    update card

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['index.html', 'src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+      },
+    },
   },
   plugins: [],
 }
